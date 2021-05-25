86bb5d6720f0680bd8f663b4e29809db14e9ce54
AvielCo <avielcohen15@gmail.com>
2021-05-25 15:01:36 +0300

    Added a function that checks which user answers are correct. Any incorrect answers are inserted into the wrongAnswers set with the questionIndex.

--- lib/questions.dart
+++ lib/questions.dart
@@ -6,22 +6,28 @@ class Questions extends StatefulWidget {
   final quiz = [
     {
       'question': 'Question 1',
-      'options': ['a', 'b', 'c'],
+      'options': ['option #1', 'option #2', 'option #3'],
       'correct': 1
     },
     {
       'question': 'Question 2',
-      'options': ['a', 'b', 'c', 'd', 'e', 'z'],
+      'options': ['option #1', 'option #2', 'option #3', 'option #4'],
       'correct': 0
     },
     {
       'question': 'Question 3',
-      'options': ['a', 'b', 'c', 'e'],
+      'options': ['option #1', 'option #2', 'option #3'],
       'correct': 2
     },
     {
       'question': 'Question 4',
-      'options': ['a', 'b', 'c', 'd'],
+      'options': [
+        'option #1',
+        'option #2',
+        'option #3',
+        'option #4',
+        'option #5'
+      ],
       'correct': 1
     },
   ];
@@ -30,12 +36,35 @@ class Questions extends StatefulWidget {
 }
 
 class _QuestionsState extends State<Questions> {
-  Map<int, int> answers = {
-    // {questionId: userAnswer}
-  };
+  // will hold the answers of the user.
+  // an entry will look like: questionIndex: answerIndex
+  // using this answers Map later when click on check answers.
+  Map<int, int> answers = Map();
+
+  // will hold the question index that the user chose the wrong answers.
+  // if question index is not appear in this set, the user's answer is correct.
+  Set<int> wrongAnswers = Set();
 
   void updateAnswer(int questionIndex, int answerIndex) {
-    answers.update(questionIndex, (value) => answerIndex, ifAbsent: () => answerIndex);
+    answers.update(questionIndex, (value) => answerIndex,
+        ifAbsent: () => answerIndex);
+  }
+
+  void checkAnswers() {
+    int amountOfQuestions = widget.quiz.length;
+    int amountOfUserAnswers = answers.length;
+    if (amountOfQuestions != amountOfUserAnswers) {
+      return;
+    }
+
+    widget.quiz.asMap().entries.forEach((question) {
+      int correctAnswer = question.value['correct'] as int;
+      int userAnswer = answers[question.key] as int;
+
+      if (correctAnswer != userAnswer) {
+        wrongAnswers.add(question.key);
+      }
+    });
   }
 
   @override
@@ -50,7 +79,7 @@ class _QuestionsState extends State<Questions> {
           ],
         );
       }).toList(),
-      ElevatedButton(onPressed: null, child: Text("Check answers"))
+      ElevatedButton(onPressed: checkAnswers, child: Text("Check answers"))
     ]);
   }
 }
